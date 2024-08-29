759a2b7cb0b73dbfa20c2d18aa2135e504b549ce
NarenKarthikBM <85060104+NarenKarthikBM@users.noreply.github.com>
2024-08-29 17:58:08 +0530

    Update Registration.jsx

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -44,10 +44,19 @@ function Register() {
   } = useForm({
     mode: "onBlur",
     defaultValues: {
+      name1: "",
+      name2: "",
+      name3: "",
+      name4: "",
+      name5: "",
+      name6: "",
       email1: "",
       email2: "",
       email3: "",
       email4: "",
+      email5: null,
+      email6: null,
+      mobile1: "",
       teamName: "",
     },
   });
@@ -109,6 +118,28 @@ function Register() {
             />
             <Divider />
             <Controller
+              name="name1"
+              control={control}
+              rules={{
+                required: true,
+                validate: {
+                  duplicateEmails: (value, formValues) => value !== formValues.name2 || value !== formValues.name3 || value !== formValues.name4,
+                },
+              }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  placeholder="Enter name of team leader"
+                  variant="bordered"
+                  value={value}
+                  onBlur={onBlur}
+                  onChange={onChange}
+                  onClear={() => setValue("name1", "")}
+                  errorMessage={errors.name1 && "Name is required"}
+                  validationState={errors.name1 ? "invalid" : "valid"}
+                />
+              )}
+            />
+            <Controller
               name="email1"
               control={control}
               rules={{
@@ -132,7 +163,49 @@ function Register() {
                 />
               )}
             />
+            <Controller
+              name="mobile1"
+              control={control}
+              rules={{
+                required: true,
+              }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  placeholder="Enter email of team leader"
+                  variant="bordered"
+                  value={value}
+                  onBlur={onBlur}
+                  onChange={onChange}
+                  onClear={() => setValue("mobile1", "")}
+                  errorMessage={errors.mobile1 && "Mobile is required"}
+                  validationState={errors.mobile1 ? "invalid" : "valid"}
+                />
+              )}
+            />
+            <Divider />
 
+            <Controller
+              name="name2"
+              control={control}
+              rules={{
+                required: true,
+                validate: {
+                  duplicateEmails: (value, formValues) => value !== formValues.name1 || value !== formValues.name3 || value !== formValues.name4,
+                },
+              }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  placeholder="Enter name of member 1"
+                  variant="bordered"
+                  value={value}
+                  onBlur={onBlur}
+                  onChange={onChange}
+                  onClear={() => setValue("name2", "")}
+                  errorMessage={errors.name2 && "Name is required"}
+                  validationState={errors.name2 ? "invalid" : "valid"}
+                />
+              )}
+            />
             <Controller
               name="email2"
               control={control}
@@ -157,7 +230,30 @@ function Register() {
                 />
               )}
             />
+            <Divider />
 
+            <Controller
+              name="name3"
+              control={control}
+              rules={{
+                required: true,
+                validate: {
+                  duplicateEmails: (value, formValues) => value !== formValues.name1 || value !== formValues.name2 || value !== formValues.name4,
+                },
+              }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  placeholder="Enter name of member 2"
+                  variant="bordered"
+                  value={value}
+                  onBlur={onBlur}
+                  onChange={onChange}
+                  onClear={() => setValue("name3", "")}
+                  errorMessage={errors.name3 && "Name is required"}
+                  validationState={errors.name3 ? "invalid" : "valid"}
+                />
+              )}
+            />
             <Controller
               name="email3"
               control={control}
@@ -178,11 +274,34 @@ function Register() {
                   onChange={onChange}
                   onClear={() => setValue("email3", "")}
                   errorMessage={errors.email3 && "Email is required"}
-                  validationState={errors.email2 ? "invalid" : "valid"}
+                  validationState={errors.email3 ? "invalid" : "valid"}
                 />
               )}
             />
+            <Divider />
 
+            <Controller
+              name="name4"
+              control={control}
+              rules={{
+                required: true,
+                validate: {
+                  duplicateEmails: (value, formValues) => value !== formValues.name1 || value !== formValues.name2 || value !== formValues.name3,
+                },
+              }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  placeholder="Enter name of member 3"
+                  variant="bordered"
+                  value={value}
+                  onBlur={onBlur}
+                  onChange={onChange}
+                  onClear={() => setValue("name4", "")}
+                  errorMessage={errors.name4 && "Name is required"}
+                  validationState={errors.name4 ? "invalid" : "valid"}
+                />
+              )}
+            />
             <Controller
               name="email4"
               control={control}
@@ -202,8 +321,98 @@ function Register() {
                   onBlur={onBlur}
                   onChange={onChange}
                   onClear={() => setValue("email4", "")}
-                  errorMessage={errors.email3 && "Email is required"}
-                  validationState={errors.email2 ? "invalid" : "valid"}
+                  errorMessage={errors.email4 && "Email is required"}
+                  validationState={errors.email4 ? "invalid" : "valid"}
+                />
+              )}
+            />
+            <Divider />
+
+            <Controller
+              name="name5"
+              control={control}
+              rules={{
+                validate: {
+                  duplicateEmails: (value, formValues) => value !== formValues.name1 || value !== formValues.name2 || value !== formValues.name3,
+                },
+              }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  placeholder="Enter name of member 4"
+                  variant="bordered"
+                  value={value}
+                  onBlur={onBlur}
+                  onChange={onChange}
+                  onClear={() => setValue("name5", "")}
+                  errorMessage={errors.name5 && "Name is required"}
+                  validationState={errors.name5 ? "invalid" : "valid"}
+                />
+              )}
+            />
+            <Controller
+              name="email5"
+              control={control}
+              rules={{
+                validate: {
+                  iiserb: (value) => value.includes("24@iiserb.ac.in"),
+                },
+              }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  placeholder="Enter email of member 4"
+                  variant="bordered"
+                  type="email"
+                  value={value}
+                  onBlur={onBlur}
+                  onChange={onChange}
+                  onClear={() => setValue("email5", "")}
+                  errorMessage={errors.email5 && "Email is required"}
+                  validationState={errors.email5 ? "invalid" : "valid"}
+                />
+              )}
+            />
+            <Divider />
+
+            <Controller
+              name="name6"
+              control={control}
+              rules={{
+                validate: {
+                  duplicateEmails: (value, formValues) => value !== formValues.name1 || value !== formValues.name2 || value !== formValues.name3,
+                },
+              }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  placeholder="Enter name of member 5"
+                  variant="bordered"
+                  value={value}
+                  onBlur={onBlur}
+                  onChange={onChange}
+                  onClear={() => setValue("name6", "")}
+                  errorMessage={errors.name6 && "Name is required"}
+                  validationState={errors.name6 ? "invalid" : "valid"}
+                />
+              )}
+            />
+            <Controller
+              name="email6"
+              control={control}
+              rules={{
+                validate: {
+                  iiserb: (value) => value.includes("24@iiserb.ac.in"),
+                },
+              }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  placeholder="Enter email of member 5"
+                  variant="bordered"
+                  type="email"
+                  value={value}
+                  onBlur={onBlur}
+                  onChange={onChange}
+                  onClear={() => setValue("email6", "")}
+                  errorMessage={errors.email6 && "Email is required"}
+                  validationState={errors.email6 ? "invalid" : "valid"}
                 />
               )}
             />
